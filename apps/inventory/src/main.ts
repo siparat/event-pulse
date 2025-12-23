87cb2776c1245737b8b001f97b760c7948138741
siparat <akimov1712@yandex.ru>
2025-12-23 22:40:46 +0300

    feat(inventory): connect inventory microservice to rmq

--- apps/inventory/src/main.ts
+++ apps/inventory/src/main.ts
@@ -1,14 +1,17 @@
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ConfigService } from '@nestjs/config';
+import { MicroserviceOptions } from '@nestjs/microservices';
+import { getRmqConfig, RmqQueues } from '@event-pulse/infrastructure';
 
 async function bootstrap(): Promise<void> {
 	const app = await NestFactory.create(AppModule);
-	const globalPrefix = 'api';
-	app.setGlobalPrefix(globalPrefix);
-	const port = process.env.PORT || 3000;
-	await app.listen(port);
-	Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+	const config = app.get(ConfigService);
+	const rmqConfig = getRmqConfig(config, RmqQueues.INVENTORY);
+	app.close();
+
+	const microservice = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, rmqConfig);
+	await microservice.listen();
 }
 
 bootstrap();
