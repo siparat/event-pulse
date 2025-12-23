8890d7bf874d38b24ebaca61c3c39366a6455852
siparat <akimov1712@yandex.ru>
2025-12-23 22:40:31 +0300

    feat(sales): connect sales microservice to rmq

--- apps/sales/src/app/app.module.ts
+++ apps/sales/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-	imports: []
+	imports: [ConfigModule.forRoot({ envFilePath: '/envs/.sales.env', isGlobal: true })]
 })
 export class AppModule {}
