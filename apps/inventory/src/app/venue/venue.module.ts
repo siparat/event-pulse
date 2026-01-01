c1a3661b947008181516ae520d3ea6b42d466627
siparat <akimov1712@yandex.ru>
2026-01-01 15:18:11 +0300

    fix(inventory): connected VenueController & CreateVenueHandler

--- apps/inventory/src/app/venue/venue.module.ts
+++ apps/inventory/src/app/venue/venue.module.ts
@@ -5,10 +5,13 @@ import { SeatModel } from './infrastructure/database/write/models/seat.model';
 import { ZoneModel } from './infrastructure/database/write/models/zone.model';
 import { VenueModel } from './infrastructure/database/write/models/venue.model';
 import { VenueRepository } from './infrastructure/database/write/repositories/venue.repository';
+import { VenueController } from './interface/rmq/venue.controller';
+import { CreateVenueHandler } from './application/create-venue/create-venue.handler';
 
 @Module({
 	imports: [TypeOrmModule.forFeature([SeatModel, ZoneModel, VenueModel])],
-	providers: [SeatRepository, VenueRepository],
+	controllers: [VenueController],
+	providers: [SeatRepository, VenueRepository, CreateVenueHandler],
 	exports: [SeatRepository]
 })
 export class VenueModule {}
