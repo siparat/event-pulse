4ecfece1a53877540320927f9cd452013e9fcac2
siparat <akimov1712@yandex.ru>
2026-01-02 14:44:19 +0300

    refactor(inventory): reorganize venue commands structure

--- apps/inventory/src/app/venue/application/commands/create-venue/create-venue.command.ts
+++ apps/inventory/src/app/venue/application/commands/create-venue/create-venue.command.ts
@@ -1,5 +1,5 @@
 import { Command } from '@nestjs/cqrs';
-import { Venue } from '../../domain/venue.aggregate';
+import { Venue } from '../../../domain/venue.aggregate';
 
 export class CreateVenueCommand extends Command<Pick<Venue, 'id'>> {
 	constructor(
